479ca2bf03a8d838e45b888aa877f8b026d5078b
danielmustard <danielmustard1@icloud.com>
2022-07-25 15:51:24 +0100

    main part working, need to add ability to increase grid size

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,39 @@
 
-
-
 //Getting grid container div
 gridContainer = document.getElementById('grid-container');
+
+const gridChildren = document.getElementsByClassName("gridChild")
+
+
+
+console.log(gridChildren);
+
+let makeGrid = () =>{
+    for (let i = 1; i < 257; i++) {
+        const div = document.createElement("div");
+        gridContainer.appendChild(div);
+        div.className = "gridChild";
+        div.setAttribute('id', `gridChild${i}`)
+      }
+}
+
+window.onload = makeGrid();
+
+//Changing div colour on mouse over
+let changeColour = (e) =>{
+      let divToChange = e.toElement;
+      divToChange.style.background="black";      
+}
+
+//Adding event listener to each div 
+Array.from(gridChildren).forEach(child => child.addEventListener('mouseover',changeColour));
+
+//Clear style from div for clear button
+let clearGrid = () =>{
+    Array.from(gridChildren).forEach(child => child.style.background="white");
+}
+
+//Size of Grid Slider
+document.getElementById("gridSize").addEventListener('input', (e) =>{
+    console.log(e.target.value);
+});
