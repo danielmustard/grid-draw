4e3d9e0855a9db42c4ea5ff5a5bc52c3fcce8d4c
danielmustard <danielmustard1@icloud.com>
2022-07-26 14:22:09 +0100

    now able to set grid size and graw on it

--- javascript/main.js
+++ javascript/main.js
@@ -1,23 +1,33 @@
-
 //Getting grid container div
 gridContainer = document.getElementById('grid-container');
-
+//Collecting each div element that is inside the grid
 const gridChildren = document.getElementsByClassName("gridChild")
 
+//Function that makes grid
+let makeGrid = (size) =>{
+    
+    //Removing leftover div's from last time function ran
+      let board = document.querySelector(".gridContainer");
+      let squares = board.querySelectorAll('div');
+      squares.forEach(div => div.remove());
 
-
-console.log(gridChildren);
-
-let makeGrid = () =>{
-    for (let i = 1; i < 257; i++) {
+    let amount = size * size; 
+    for (let i = 0; i < amount; i++) {
         const div = document.createElement("div");
         gridContainer.appendChild(div);
         div.className = "gridChild";
         div.setAttribute('id', `gridChild${i}`)
+        //Making grid set size
+        gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+        gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
       }
+      Array.from(gridChildren).forEach(child => child.addEventListener('mouseover',changeColour));
 }
 
-window.onload = makeGrid();
+//function that makes new grid of size set by user
+let changeSize = (input) => {
+    makeGrid(input);
+} 
 
 //Changing div colour on mouse over
 let changeColour = (e) =>{
@@ -25,15 +35,8 @@ let changeColour = (e) =>{
       divToChange.style.background="black";      
 }
 
-//Adding event listener to each div 
-Array.from(gridChildren).forEach(child => child.addEventListener('mouseover',changeColour));
 
 //Clear style from div for clear button
 let clearGrid = () =>{
     Array.from(gridChildren).forEach(child => child.style.background="white");
 }
-
-//Size of Grid Slider
-document.getElementById("gridSize").addEventListener('input', (e) =>{
-    console.log(e.target.value);
-});
